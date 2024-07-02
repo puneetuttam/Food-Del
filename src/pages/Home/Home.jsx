decc8f13297b6da641d68777fe54b2b946561a37
Puneet Uttam <puneet.uttam.99@gmail.com>
2024-07-02 08:08:32 +0530

    FoodItem component added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,14 @@ import "./Home.css";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import Header from "../../components/Header/Header";
 import { useState } from "react";
-// import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 const Home = () => {
   const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
-      {/* <FoodDisplay category={category} /> */}
+      <FoodDisplay category={category} />
     </div>
   );
 };
